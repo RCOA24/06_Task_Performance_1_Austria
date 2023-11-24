5de4c823de5601df36a74aba4ef098ca714bf909
RCOA24 <mrtangkad59@gmail.com>
2023-11-24 17:00:54 +0800

    Add project files.

---
+++ 06_Task_Performance_1_Austria/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_Task_Performance_1_Austria
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
